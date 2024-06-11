7a0cf26e9caaca6048dd094c50eb4231c0816e89
winternotseason <winternotseason@naver.com>
2024-06-11 16:39:48 +0900

    Detail Movie 컴포넌트로 분리

---
+++ src/components/DetailHeader.jsx
@@ -0,0 +1,114 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const StyledDetailHeader = styled.header`
+  height: 70rem;
+  background-image: url(${(props) => props.backdrop});
+  background-repeat: no-repeat;
+  background-size: cover;
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 22rem;
+    width: 100%;
+    height: 70rem;
+    background-color: rgba(
+      0,
+      0,
+      0,
+      0.4
+    ); /* 투명도 조절을 위해 alpha 값을 조절할 수 있습니다. */
+  }
+  .content {
+    color: #ffffff;
+    position: relative;
+    z-index: 10;
+    top: 45rem;
+    h1,
+    p {
+      margin: 0;
+    }
+    h1 {
+      font-size: 5rem;
+      padding: 0 5rem;
+    }
+
+    .flex {
+      margin-top: 1.5rem;
+      display: flex;
+      align-items: center;
+      .btn {
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        margin-left: 5rem;
+        button {
+          background-color: white;
+          font-size: 1.3rem;
+          border: 1px solid black;
+          width: 18rem;
+          height: 4rem;
+          margin: 0.1rem;
+          border-radius: 8px;
+          font-weight: 600;
+          cursor: pointer;
+        }
+      }
+      .dec {
+        .sub {
+          margin-top: 1rem;
+        }
+        p {
+          font-weight: 400;
+          font-size: 1.1rem;
+          margin-left: 2rem;
+          margin-right: 5rem;
+        }
+      }
+    }
+  }
+`;
+
+const DetailHeader = ({ movie }) => {
+  return (
+    <StyledDetailHeader
+      backdrop={`https://image.tmdb.org/t/p/w1280${movie.drop}`}
+    >
+      <div className="content">
+        <h1>{movie.title}</h1>
+        <div className="flex">
+          <div className="btn">
+            <button>바로예매</button>
+            <button>관심영화</button>
+          </div>
+          <div className="dec">
+            <p>{movie.dec} </p>
+            <p className="sub">
+              {movie.date && movie.date} | {movie.genre} | {movie.runtime}분 |{" "}
+              {movie.actors &&
+                Array.isArray(movie.actors) &&
+                movie.actors.join(", ")}{" "}
+              외
+            </p>
+          </div>
+        </div>
+      </div>
+    </StyledDetailHeader>
+  );
+};
+
+DetailHeader.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    dec: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    drop: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    runtime: PropTypes.number.isRequired,
+    actors: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }).isRequired,
+};
+
+export default DetailHeader;
--- src/pages/DetailMovie.jsx
+++ src/pages/DetailMovie.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
+import DetailHeader from "../components/DetailHeader";
 import Loading from "../components/Loading";
 import { getMovieCredit, getMovieDetail } from "../services/api";
 
@@ -16,80 +17,12 @@ const StyledDiv = styled.div`
   }
 `;
 
-const StyledHeader = styled.header`
-  height: 70rem;
-  background-image: url(${(props) => props.backdrop});
-  background-repeat: no-repeat;
-  background-size: cover;
-  &::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 22rem;
-    width: 100%;
-    height: 70rem;
-    background-color: rgba(
-      0,
-      0,
-      0,
-      0.4
-    ); /* 투명도 조절을 위해 alpha 값을 조절할 수 있습니다. */
-  }
-  .content {
-    color: #ffffff;
-    position: relative;
-    z-index: 10;
-    top: 45rem;
-    h1,
-    p {
-      margin: 0;
-    }
-    h1 {
-      font-size: 5rem;
-      padding: 0 5rem;
-    }
-
-    .flex {
-      margin-top: 1.5rem;
-      display: flex;
-      align-items: center;
-      .btn {
-        display: flex;
-        flex-direction: column;
-        text-align: center;
-        margin-left: 5rem;
-        button {
-          background-color: white;
-          font-size: 1.3rem;
-          border: 1px solid black;
-          width: 18rem;
-          height: 4rem;
-          margin: 0.1rem;
-          border-radius: 8px;
-          font-weight: 600;
-          cursor: pointer;
-        }
-      }
-      .dec {
-        .sub {
-          margin-top: 1rem;
-        }
-        p {
-          font-weight: 400;
-          font-size: 1.1rem;
-          margin-left: 2rem;
-          margin-right: 5rem;
-        }
-      }
-    }
-  }
-`;
-
 const DetailMovie = () => {
   const [loading, setLoading] = useState(false);
   const [searchParams] = useSearchParams();
   const query = searchParams.get("query");
-  const [movie, setMovie] = useState([]);
+  const [movie, setMovie] = useState({});
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -100,7 +33,6 @@ const DetailMovie = () => {
         const movieObj = {
           title: res.title,
           dec: res.overview,
-          popularity: res.popularity,
           date: res.release_date,
           poster: res.poster_path,
           drop: res.backdrop_path,
@@ -124,28 +56,7 @@ const DetailMovie = () => {
   }
   return (
     <StyledDiv>
-      <StyledHeader
-        backdrop_id={movie.drop}
-        backdrop={`https://image.tmdb.org/t/p/w1280${movie.drop}`}
-        poster={`https://image.tmdb.org/t/p/w500${movie.poster}`}
-      >
-        <div className="content">
-          <h1>{movie.title}</h1>
-          <div className="flex">
-            <div className="btn">
-              <button>바로예매</button>
-              <button>관심영화</button>
-            </div>
-            <div className="dec">
-              <p>{movie.dec} </p>
-              <p className="sub">
-                {movie.date && movie.date.slice(0, 4)} | {movie.genre} |{" "}
-                {movie.runtime}분 | {movie.actors && movie.actors.join(", ")} 외
-              </p>
-            </div>
-          </div>
-        </div>
-      </StyledHeader>
+      <DetailHeader movie={movie} />
     </StyledDiv>
   );
 };
