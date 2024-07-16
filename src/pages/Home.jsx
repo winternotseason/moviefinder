c44cd0811a23738e3e9ca1b19e15c6b9226ec2a3
winternotseason <winternotseason@naver.com>
2024-07-17 04:05:11 +0900

    헤더 반응형 폰트 크기

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div className="w-full h-full">
       <div className="w-full flex flex-col justify-center items-center">
-        <div className="w-full relative">
+        <div className="relative w-full max-w-[90rem] min-w-[40rem]">
           <ReactPlayer
             url="/videos/mainvideo.mp4"
             muted
@@ -21,17 +21,17 @@ const Home = () => {
             }}
           />
           <img src={gradient} className="w-full absolute top-0 left-0 h-full" />
-          <div className="absolute top-80 left-20 text-white z-50">
-            <p className="text-5xl font-bold mb-4">파묘</p>
-            <p className="text-2xl font-light">
+          <div className="absolute top-1/2 md:top-2/4 left-10 md:left-20 text-white z-50">
+            <p className="text-3xl md:text-5xl font-bold mb-2 md:mb-4">파묘</p>
+            <p className="md:text-2xl font-light">
               새로운 맛의 <span className="font-semibold">K 오컬트</span> 탄생,
-              2024년 첫 천만 영화!
+              올해 첫 천만 영화!
             </p>
-            <p className="text-2xl font-light">
+            <p className="md:text-2xl font-light">
               수상한 묘를 이장한 풍수사와 장의사, 무속인들에게 벌어지는 기이한
               사건
             </p>
-            <p className="text-sm mt-2">
+            <p className="text-xs md:text-sm mt-1 md:mt-2">
               장재헌 감독, 최민식, 김고은, 유해진, 이도헌 외
             </p>
           </div>
