96e138dbff812b9bf00f9b6c8db712984c3a5f02
winternotseason <winternotseason@naver.com>
2024-07-18 19:49:41 +0900

    search rank창 min-height

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -18,65 +18,72 @@ const Search = () => {
     };
     fetchRank();
   }, []);
+
   if (loading) {
-    return <LoadingSpinner color="#f56363"/>;
+    return (
+      <div className="min-h-[30rem] md:min-h-[40rem]">
+        <LoadingSpinner color="#f56363" />
+      </div>
+    );
   }
   return (
-    <div>
-      {/* input 공간 */}
-      <div className="py-3 px-5 border-b-[12px] border-black/5">
-        <form
-          className="flex items-center"
-          onSubmit={(e) => {
-            e.preventDefault();
-            navigate(`/search/list?query=${inputValue}`);
-            setInputValue("");
-          }}
-        >
-          <input
-            type="text"
-            value={inputValue}
-            onChange={(e) => {
-              setInputValue(e.target.value);
+    <div className="w-full flex justify-center">
+      <div className="w-full max-w-[70rem] ">
+        {/* input 공간 */}
+        <div className="py-3 px-5 border-b-[12px] border-black/5 md:hidden">
+          <form
+            className="flex items-center"
+            onSubmit={(e) => {
+              e.preventDefault();
+              navigate(`/search/list?query=${inputValue}`);
+              setInputValue("");
             }}
-            placeholder="검색어를 입력해주세요."
-            className="w-full font-light outline-none"
-          />
-          {inputValue && (
-            <TiDeleteOutline
-              size={25}
-              color="#9b9b9b"
-              onClick={() => {
-                setInputValue("");
+          >
+            <input
+              type="text"
+              value={inputValue}
+              onChange={(e) => {
+                setInputValue(e.target.value);
               }}
+              placeholder="검색어를 입력해주세요."
+              className="w-full font-light outline-none"
             />
-          )}
-          <button>
-            <CiSearch size={25} className="ml-4" />
-          </button>
-        </form>
-      </div>
-      {/* 메인 공간 */}
-      <div>
-        <ul className="w-full flex flex-col px-5">
-          <h1 className="py-5 text-sm font-semibold">오늘의 영화</h1>
-          {movieRank &&
-            movieRank.map((movie) => (
-              <li
-                key={movie.rank}
-                className="flex py-3 text-sm border-b-[1px] border-black/10"
-              >
-                <p className="flex w-7 justify-center font-semibold text-red-500">
-                  {movie.rank}
-                </p>
-                <Link
-                  to={`/detail?moviename=${movie.movieNm}&release=${movie.repRlsDate}`}
+            {inputValue && (
+              <TiDeleteOutline
+                size={25}
+                color="#9b9b9b"
+                onClick={() => {
+                  setInputValue("");
+                }}
+              />
+            )}
+            <button>
+              <CiSearch size={25} className="ml-4" />
+            </button>
+          </form>
+        </div>
+        {/* 메인 공간 */}
+        <div>
+          <ul className="w-full flex flex-col px-5 pb-10">
+            <h1 className="py-5 text-sm font-semibold md:text-2xl">오늘의 영화</h1>
+            {movieRank &&
+              movieRank.map((movie) => (
+                <li
+                  key={movie.rank}
+                  className="flex py-3 md:py-7 text-sm md:text-xl border-b-[1px] border-black/10"
                 >
-                  <p className="ml-2">{movie.movieNm}</p>
-                </Link>
-              </li>
-            ))}
-        </ul>
+                  <p className="flex w-7 justify-center font-semibold text-red-500">
+                    {movie.rank}
+                  </p>
+                  <Link
+                    to={`/detail?moviename=${movie.movieNm}&release=${movie.repRlsDate}`}
+                  >
+                    <p className="ml-2">{movie.movieNm}</p>
+                  </Link>
+                </li>
+              ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
